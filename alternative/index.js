d71a0ca2b3d36682c0f1f207a6fd66fef9409307
M Haidar Hanif <git@mhaidarhanif.com>
2023-02-05 09:22:48 +0700

    feat: formSearchElement handle submit event

--- alternative/index.js
+++ alternative/index.js
@@ -1,1 +1,11 @@
-console.log("Test");
+const formSearchElement = document.getElementById("form-search");
+
+formSearchElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(formSearchElement);
+
+  const keyword = formData.get("keyword");
+
+  console.log({ keyword });
+});
